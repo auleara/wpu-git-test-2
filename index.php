04e5a7c4ae03c165b9dede496d8e1614b7948888
auleara <nurrizkytaaulia@gmail.com>
2020-10-29 08:43:50 -0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Git REMOTE</title>
+</head>
+<body>
+<p>Bikin INDEX PHP</p>
+</body>
+</html>
