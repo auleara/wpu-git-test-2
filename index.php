b3b96fb601f75945c7b8a9a6c0664cc14b2b3c7d
auleara <nurrizkytaaulia@gmail.com>
2020-10-29 08:44:28 -0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
 <p>Bikin INDEX PHP</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente numquam eius quibusdam natus, dicta officia commodi libero quis ratione delectus temporibus ad eveniet amet. Facere quidem voluptatum autem praesentium ipsam?</p>
 </body>
 </html>
